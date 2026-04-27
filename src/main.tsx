371886d1866c021219c14ce82688963335a6e7d0
Amirbek_Amonov <amonovamirbek1@gmail.com>
2026-04-27 10:57:12 +0500

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 import './index.css';
 import './shared/i18n';
@@ -8,7 +8,7 @@ import { initializeLocale } from './shared/lib/language';
 initializeLocale();
 
 createRoot(document.getElementById('root')!).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
